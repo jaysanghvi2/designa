e281dcd52318e7f06a687fbe18a3ed9d20f92f34
jaysanghvi2 <65889727+jaysanghvi2@users.noreply.github.com>
2023-01-30 20:45:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+import Main from './main/main'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+
+function App() {
+  return (
+    <div >
+      <Main />
+    </div>
+  );
+}
+
+export default App;
